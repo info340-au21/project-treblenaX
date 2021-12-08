7309906427cc92d7417161e7fdb3a7b30022c737
Ryan Langford <ryanclangford@icloud.com>
2021-12-07 20:14:13 -0800

    moved footer to bottom of page

--- src/components/FooterModule.js
+++ src/components/FooterModule.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function FooterModule() {
     return (
-        <footer>Created with love by Elbert Cheng, Ryan Langford, Stephan Rubalcava, Alan Wen</footer>
+        <footer className="footer">Created with love by Elbert Cheng, Ryan Langford, Stephan Rubalcava, Alan Wen</footer>
     );
 }
